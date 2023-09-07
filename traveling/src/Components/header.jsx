6e22263dbdfd11920b93d11b4f6a3363d5804f30
saraibaqeel1 <Saraib.sidtechno@gmail.com>
2023-09-08 02:27:54 +0500

    saraib

---
+++ traveling/src/Components/header.jsx
@@ -0,0 +1,85 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import Countdown from 'react-countdown';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState,useEffect } from 'react';
+import CountdownComponent from './Countdown';
+import { AiOutlineInstagram } from 'react-icons/ai';
+
+import {FiTwitter,FiFacebook,FiInstagram} from 'react-icons/fi'
+
+function Header() {
+    const [count, setCount] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+    const targetTimestamp = Date.now() + 7 * 24 * 60 * 60 * 1000;
+    useEffect(() => {
+        const interval = setInterval(() => {
+          const currentTime = Date.now();
+          const remainingTime = targetTimestamp - currentTime;
+    
+          if (remainingTime <= 0) {
+            clearInterval(interval);
+            return;
+          }
+    
+          const seconds = Math.floor((remainingTime / 1000) % 60);
+          const minutes = Math.floor((remainingTime / 1000 / 60) % 60);
+          const hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
+          const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    
+          setCount({ days, hours, minutes, seconds });
+        }, 1000);
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, []);
+      const formatNumber = (num) => {
+        // Add leading zeros to numbers less than 10 for a consistent width
+        return num < 10 ? `0${num}` : num;
+      };
+   
+
+  return (
+    <div className='herosection'>
+        <div className='icon-box'>
+            <div> <FiInstagram className='icons' /></div>
+       
+            <div>  <FiFacebook className='icons'/></div>
+            <div>     <FiTwitter className='icons'/></div>
+      
+   
+        </div>
+    <Navbar expand="lg" className="bg-body-tertiary Nav-bar">
+      <Container fluid>
+        <Navbar.Brand  href="#" className="Nav-bar">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            
+          
+          </Nav>
+          <Form className="d-flex nav-rightdiv">
+           <div className='nav-rightlist'>Home</div>
+             <div className='nav-rightlist'>About Us</div>
+             <div className='nav-rightlist'>Contacts</div>
+           
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <h3 className='comingsoon' >Our online store will open in....</h3>
+<CountdownComponent/>
+    </div>
+  );
+}
+
+export default Header;
---
+++ traveling/src/Pages/Landingpage.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import Header from '../Components/header';
+
+function Landingpage(){
+return(
+    <div>
+  <Header/>
+    </div>
+)
+}
+export default Landingpage;
